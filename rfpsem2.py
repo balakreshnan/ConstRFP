48ee4d855b01b9e6720e14a720a13080c4c58d89
balakreshnan <babal@microsoft.com>
2024-11-04 10:24:55 -0600

    updated

--- rfpsem2.py
+++ rfpsem2.py
@@ -2,6 +2,7 @@
 #on local machine run: streamlit run 001_jokewebapp.py
 
 import base64
+import logging
 from uuid import uuid4
 from pandas import DataFrame
 import streamlit as st
@@ -10,6 +11,7 @@ from typing import Annotated
 import os
 from semantic_kernel.agents import ChatCompletionAgent
 from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+from semantic_kernel.utils.logging import setup_logging
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 from semantic_kernel.contents.chat_history import ChatHistory
 from semantic_kernel.contents.utils.author_role import AuthorRole
@@ -92,13 +94,23 @@ async def agenticrfp():
 
     service_id = "agent"
 
-    kernel.add_service(AzureChatCompletion(service_id=service_id))
+    # kernel.add_service(AzureChatCompletion(service_id=service_id))
+    # Add Azure OpenAI chat completion
+    chat_completion = AzureChatCompletion(
+        deployment_name=os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        base_url=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    )
+    kernel.add_service(chat_completion)
+    # Set the logging level for  semantic_kernel.kernel to DEBUG.
+    setup_logging()
+    logging.getLogger("kernel").setLevel(logging.DEBUG)
 
     # kernel.add_plugin(plugin=YourPlugin(), plugin_name="your_plugin")
-    kernel.add_plugin(rfpchat(),plugin_name="RFP_Chat",)
-    kernel.add_plugin(historyrfpchat(),plugin_name="HistoryRFPContent_Chat",)
+    kernel.add_plugin(rfpchat(),plugin_name="RFP_Chat", description="This plugin provides the ability to chat about RFPs, PDF will be uploaded")
+    kernel.add_plugin(historyrfpchat(),plugin_name="HistoryRFPContent_Chat", description="This plugin provides the ability to chat about historical RFPs")
 
-    agent = ChatCompletionAgent(service_id=service_id, kernel=kernel, name="RFP Agent", instructions="<agent instructions>")
+    agent = ChatCompletionAgent(service_id=service_id, kernel=kernel, name="RFP Agent", instructions="You are AI Agent to provide knowledge about RFPs, based on the question pick the right agent to response. There are 1 one for rfp uploaded document and other for historical")
 
     # Define a thread and invoke the agent with the user input
     # thread_id = await agent.create_thread()
